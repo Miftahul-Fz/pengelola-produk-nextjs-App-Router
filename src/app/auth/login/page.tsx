01e4ee303da8df5f91401a02ec608d061474df76
3202102012 <miftahulwilna@gmail.com>
2023-11-02 16:50:52 +0700

    update templere bootstrap

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,5 @@
 "use client"
-import React, { FormEvent, useState } from 'react';
-import { MDBContainer, MDBInput, MDBBtn } from 'mdb-react-ui-kit';
+import React, { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -35,32 +34,51 @@ export default function Login() {
     }
   }
 
+
+
   return (
-    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
-      <h3 style={{fontFamily: "times new roman"}}>FORM LOGIN</h3>
-      <form onSubmit={handleLogin}>
-        <MDBInput
-          wrapperClass='mb-4'
-          label='Username'
-          type='text'
-          name='username'
-          value={formData.username}
-          onChange={handleInputChange}
-        />
-        <MDBInput
-          wrapperClass='mb-4'
-          label='Password'
-          type='password'
-          name='password'
-          value={formData.password}
-          onChange={handleInputChange}
-        />
-        <MDBBtn type='submit' className="mb-4">Sign in</MDBBtn>
-      </form>
-      <div className="text-center">
-        <p>Not a member? <a href="/auth/register">Register</a></p>
-        {/* <p>or sign up with:</p> */}
+    <div className="App">
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-4">
+            <form id="loginform" onSubmit={handleLogin}>
+              <div className="form-group">
+                <label>username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="username"
+                  placeholder="masukan usrname"
+                  value={formData.username}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                 type="password"
+                 className="form-control"
+                 name="password"
+                 placeholder="masukan password"
+                 value={formData.password}
+                 onChange={handleInputChange}
+                  />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label className="form-check-label">Check me out</label>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </MDBContainer>
+    </div>
   );
 }
--- src/app/componen/navbar.tsx
+++ src/app/componen/navbar.tsx
@@ -1,36 +1,31 @@
 "use client"
 import React from 'react';
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand
-} from 'mdb-react-ui-kit';
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
   const { status }: { status: string } = useSession();
   return (
     <>
-      <MDBNavbar light bgColor='success'>
-        <MDBContainer fluid>
-          <MDBNavbarBrand  tag="span" className='mb-0 h1'>Apliksi Pengolaan Produk</MDBNavbarBrand>
-          {status === 'authenticated' ? (
-        <button 
-        className="btn btn-success btn-sm cursor-pointer" 
-        onClick={() => signOut()}
-      > 
-        LOGOUT  
-      </button>
-      ) : (
-        <button 
-        className="btn btn-success btn-sm cursor-pointer" 
-        onClick={() => signIn()}
-      > 
-        LOGIN 
-      </button>
-      )}
-        </MDBContainer>
-      </MDBNavbar>
+      <nav className="navbar fixed-top navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Aplikasi</a>
+            {status === 'authenticated' ? (
+            <button 
+            className="btn btn-success btn-sm cursor-pointer" 
+            onClick={() => signOut()}
+          > 
+            LOGOUT  
+          </button>
+          ) : (
+            <button 
+            className="btn btn-success btn-sm cursor-pointer" 
+            onClick={() => signIn()}
+          > 
+            LOGIN 
+          </button>
+          )}
+        </div>
+      </nav>
     </>
   );
 }
